76905b8ec097241264ef8535a106c312247880b4
Edmond Lam <elam3@stuy.edu>
2016-12-13 22:07:24 -0500

    Reworked CSS and how things are displayed in panels.

--- utils/newsUtils.py
+++ utils/newsUtils.py
@@ -70,11 +70,10 @@ sources = ['abc-news-au',
            'wired-de',
            'wirtschafts-woche']
 def samplenews(site,order):
-    stringres = 'https://newsapi.org/v1/articles?source=' + sources[site] + '&sortBy=' + order + '&apiKey=key'
+    stringres = 'https://newsapi.org/v1/articles?source=' + sources[site] + '&sortBy=' + order + '&apiKey=7f93124e015c4d33bbc2e336b0f2244d'
     response = urllib2.urlopen(stringres)
     data = json.load(response)
     dic = data[u'articles']
-    print dic
     return dic
 print sources[66]
 #can sort by latest, top, or popular 
